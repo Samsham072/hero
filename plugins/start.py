5ca72af3abdc1f63348b93c887429f2fe60c821e
Samsham072 <106313979+Samsham072@users.noreply.github.com>
2022-05-26 16:46:52 +0530

    Add files via upload

--- plugins/start.py
+++ plugins/start.py
@@ -1,1 +1,27 @@
+import asyncio
+from pyrogram import Client
+from pyrogram import filters
+
+from log import log
+
+
+
+
+
+@Client.on_message(filters.command("start",prefixes=['.','/','!','?'],case_sensitive=False) & filters.text)
+async def start(_, message):
+    caption = 'Hello [{}](tg://user?id={}) Thanks For Using Me.\nHit /cmds To Check Commands.'.format(message.from_user.first_name,message.from_user.id)
+    await message.reply_text(caption, quote=True)
+
+
+
+@Client.on_message(filters.command("cmds",prefixes=['.','/','!','?'],case_sensitive=False) & filters.text)
+async def cmds(_, message):
+    mess = f"""
+/chi - Auth Checker
+"""
+    await message.reply(mess, quote=True)
+   
+
+
 
